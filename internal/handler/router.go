fcde547b0d89571eb0752aeed06373931d6ccee0
Calvin Huang <calvin.peak@finalbuild.zip>
2024-07-16 13:58:00 +0800

    feat: register `/jobs` route and protect with authenticator

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"fliqt/internal/model"
 	"fliqt/internal/repository"
+	"fliqt/internal/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -11,10 +13,12 @@ func NewRouter(
 	app *gin.Engine,
 	logger *zerolog.Logger,
 	jobRepo *repository.JobRepository,
+	authService service.AuthServiceInterface,
 ) {
 	r := app.Group("/api")
 
 	jobHandler := NewJobHandler(jobRepo, logger)
 	r.GET("/jobs", jobHandler.ListJobs)
 	r.GET("/jobs/:id", jobHandler.GetJob)
+	r.POST("/jobs", AuthHandler(authService, []model.UserRole{model.RoleHR}), jobHandler.CreateJob)
 }
